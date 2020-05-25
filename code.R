8fc7cf3f20da47316af3ea74d671e96626879cde
Mira Sohn <tonton07@gmail.com>
2020-05-25 02:02:45 -0400

    code

--- code.R
+++ code.R
@@ -2,43 +2,118 @@ library(tidyverse)
 library(ggplot2)
 library(DESeq2)
 library(pheatmap)
+library(RColorBrewer)
 
-# raw count data: it has to be a matrix of integers 
-ct <- read_tsv("gilad_count_table.txt")
-ct1 <- as.matrix(ct[, 2:ncol(ct)])
-rownames(ct1) <- ct$gene
-ct2 <- apply(ct1, 2, as.integer)
 
-# metadata 
-md <- read_delim("gilad_phenodata.txt", delim = " ") %>%
-        mutate(gender = factor(gender))
-md1 <- as.matrix(md[, "gender"])
-rownames(md1) <- md$sample.id
+# supp table 
+rct <- read_csv("rawcounts.csv")
+rct1 <- rct %>% 
+        select(GeneSymbol, R1L1.HSM1:R8L2.HSF3)
+
+samples <- colnames(select(rct1, -EnsemblGeneID))
+
+rct2 <- rct1 %>%
+        gather(sample_id, counts, - EnsemblGeneID) %>%
+        mutate(sp = case_when(
+                str_detect(sample_id, "HSM") ~ "HS_Male",
+                str_detect(sample_id, "HSF") ~ "HS_Female",
+                str_detect(sample_id, "RMM") ~ "RM_Male",
+                str_detect(sample_id, "RMF") ~ "RM_Female",
+                str_detect(sample_id, "PTM") ~ "PT_Male",
+                str_detect(sample_id, "PTF") ~ "PT_Female")) %>%
+        separate(sp, c("species", "gender"), sep = "_") %>%
+        select(sample_id, species, gender)
+
+mdata <- data.frame(sample = colnames(rct1 %>% select(- EnsemblGeneID))) %>%
+        inner_join(rct2, by = c("sample" = "sample_id"))
+
+mdata1 <- unique(mdata)
+
+# metadata: mdata2 
+mdata2 <- as.matrix(select(mdata1, -sample))
+rownames(mdata2) <- mdata1$sample
+
+# raw count data: rct3
+rct3 <- as.matrix(select(rct1, - EnsemblGeneID))
+rownames(rct3) <- rct1$EnsemblGeneID
+
 
 # creating DESeq2 object des 
-des <- DESeqDataSetFromMatrix(countData = ct2,
-                              colData = md1, 
-                              design = ~ gender)
+des <- DESeqDataSetFromMatrix(countData = rct3,
+                              colData = mdata2, 
+                              design = ~ species + gender)
 
-# normalizing counts
-des <- estimateSizeFactors(des)
+# normalization
+des1 <- estimateSizeFactors(des)
+rct_norm <- counts(des1, normalized = TRUE) %>%
+        apply(2, as.integer)
 
-# extracting normalized counts 
-des <- counts(des, normalized = TRUE) 
-des <- apply(des, 2, as.integer)
+des_norm <- DESeqDataSetFromMatrix(countData = rct_norm,
+                                   colData = mdata2, 
+                                   design = ~ species + gender)
 
-
-# logarithmic transformation for quality control
-vsd <- vst(des, blind = TRUE)
-
-# hierarchical clustering 
-log_mat_des <- assays(vsd)
-log_cor_des <- cor(log_mat_des)
-
-# Quality control: PCA 
-plotPCA(des, intgroup = "gender")
+vsd <- vst(des_norm, blind = TRUE) 
 
 
+# Run DEseq 
+des_deseq <- DESeq(des_norm)
+res_names <- resultsNames(des_deseq)
+
+# extracting results
+res1 <- results(des_deseq,
+                name = "gender_Male_vs_Female",
+                alpha = 0.05)
+
+res2 <- results(des_deseq, 
+                name = "species_PT_vs_HS",
+                alpha = 0.05)
+
+res3 <- results(des_deseq,
+                name = "species_RM_vs_HS",
+                alpha = 0.05)
+
+# shrinkage
+res1_s <- lfcShrink(des_deseq,
+                  coef = "gender_Male_vs_Female",
+                  type = "apeglm") 
+
+res2_s <- lfcShrink(des_deseq,
+                    coef = "species_PT_vs_HS",
+                    type = "apeglm")  
+res3_s <- lfcShrink(des_deseq,
+                   coef = "species_RM_vs_HS",
+                   type = "apeglm")
+
+# data cleaning for plotting 
+m_vs_f <- cbind(gene = rct1$EnsemblGeneID, as.data.frame(res1_s))
 
 
+
+
+
+# modeling dispersion
+mean_counts <- rowMeans(rct_norm)
+var_counts <- rowVars(rct_norm)
+disp_df <- data.frame(mean_counts, var_counts)
+disp_plot <- ggplot(disp_df, 
+                    aes(x = mean_counts, 
+                        y = var_counts)) +
+        geom_point(alpha = 0.1) + 
+        scale_x_log10() +
+        scale_y_log10() + 
+        theme_bw()
+
+
+
+# correlation heatmap 
+htmap <- vsd %>%
+        assay() %>%
+        cor() %>%
+        pheatmap(annotation = select(as.data.frame(mdata2), c(species, gender)),
+                 main = "Correlation Heatmap")
+
+# PCA
+pca <- plotPCA(vsd, intgroup = c("gender", "species")) +
+        theme_bw() +
+        ggtitle("PCA")
 
